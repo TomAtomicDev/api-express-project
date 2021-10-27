dbf85a0b933d2b666ee77a4ecea049fc7b0b8fb7
tpenaloza <tompenaloza@gmail.com>
2021-10-27 17:09:35 -0400

    Construimos e implementamos middleware de error y de validación de datos

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const routerApi = require('./Routers+Services');//el archivo index.js se busca en automático
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());//este es un Middleware
 
@@ -14,4 +15,9 @@ app.listen(port, () => {
   console.log(`Estamos escuchando en el puerto http://localhost:${port}`)
 })
 
-routerApi(app);
+routerApi(app); // Este es el index.js de routing con express como atributo
+
+
+app.use(logErrors);//Es muy importante poner este primero xq lleva el next
+app.use(boomErrorHandler);
+app.use(errorHandler);

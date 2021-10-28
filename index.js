a0b449ab4c0fc533a2b8d877278745c93262d6ae
tpenaloza <tompenaloza@gmail.com>
2021-10-27 23:09:18 -0400

    He implementado CORS para proteger el acceso a la API

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+
 const routerApi = require('./Routers+Services');//el archivo index.js se busca en automático
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.json());//este es un Middleware
+
+const whitelist = ['http://localhost:5500', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin)  {
+      callback( null, true);
+    } else {
+      callback(new Error ('No tiene permiso para acceder'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola, este es mi primer servidor!')
 });
 
-app.listen(port, () => {
-  console.log(`Estamos escuchando en el puerto http://localhost:${port}`)
-})
 
 routerApi(app); // Este es el index.js de routing con express como atributo
 
@@ -21,3 +32,8 @@ routerApi(app); // Este es el index.js de routing con express como atributo
 app.use(logErrors);//Es muy importante poner este primero xq lleva el next
 app.use(boomErrorHandler);
 app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Estamos escuchando en el puerto http://localhost:${port}`)
+})
+

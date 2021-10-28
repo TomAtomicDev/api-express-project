b7253d22121437159d84198c926c4579560b7a0c
tpenaloza <tompenaloza@gmail.com>
2021-10-27 23:57:34 -0400

    Mejorando el Deploy

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ const options = {
 app.use(cors(options));
 
 app.get('/', (req, res) => {
-  res.send('Hola, este es mi primer servidor!')
+  res.send(`Hola! Este es el primer servidor programado por Tommy 😎💪💚
+   Fue construido sobre NodeJS usando ExpressJS.
+   Puedes probar la API en: https://obscure-bayou-83657.herokuapp.com/api/v1/products`)
 });
 
 
